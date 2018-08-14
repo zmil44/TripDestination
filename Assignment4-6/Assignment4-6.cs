1fac7aac283d93a7e2d01b2a710d79645ac68138
zmil44 <zachary.j.miller@outlook.com>
2018-08-14 12:24:44 -0400

    Initial Commit

---
+++ Assignment4-6/Assignment4-6.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Trip Destination: ");
+            string inputValue = Console.ReadLine();
+            string destination = inputValue;
+           
+            Console.Write("Enter Miles Traveled: ");
+            inputValue = Console.ReadLine();
+            double milesTraveled = double.Parse(inputValue);
+            
+            Console.Write("Enter Gallons: ");
+            inputValue = Console.ReadLine();
+            double gallons = double.Parse(inputValue);
+
+            Console.Write("Enter Total Fuel Cost: ");
+            inputValue = Console.ReadLine();
+            double totalFuelCost = double.Parse(inputValue);
+
+            Calculations newCalculation = new Calculations(destination,milesTraveled,gallons,totalFuelCost);
+            Console.WriteLine(newCalculation);
+        }
+    }
+}
---
+++ Assignment4-6/Calculations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4_6
+{
+
+    class Calculations
+    {
+        public double calculateMPG(double milesTraveled, double gallons)
+        {
+            double milesPerGallon = (milesTraveled / gallons);
+            return milesPerGallon;
+        }
+        public double calculateCostPerMile(double totalFuelCost, double milesTraveled)
+        {
+            double costPerMile = (totalFuelCost / milesTraveled);
+            return costPerMile;
+        }
+        private string destination;
+        private double milesTraveled;
+        private double gallons;
+        private double totalFuelCost;
+        private double mpg;
+        private double cost_per_mile;
+
+        public Calculations()
+        {
+        }
+
+        public Calculations(string tripDestination, double traveled, double gallons_of_fuel, double fuelCost)
+        {
+            destination = tripDestination;
+            milesTraveled = traveled;
+            gallons = gallons_of_fuel;
+            totalFuelCost = fuelCost;
+            mpg=calculateMPG(milesTraveled, gallons);
+            cost_per_mile=calculateCostPerMile(totalFuelCost,milesTraveled);
+        }
+        public override string ToString()
+        {
+            return "\nDestination: " + destination + "\nMiles Traveled: " + milesTraveled + "\nFuel Consumed: " 
+                + gallons + "\nTotal Fuel Cost: " + totalFuelCost + "\nMPG: " + mpg.ToString("f2") + "\nCost per Mile: " + cost_per_mile.ToString("c2");
+        }
+    }
+}
